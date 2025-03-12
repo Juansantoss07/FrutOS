46ab70472a692bbf6fcc637bdae013a5582cd084
Juansantoss07 <juansantosscunha@gmail.com>
2025-03-11 22:43:30 -0300

    iniciado criação da area de trabalho

--- power-pc.js
+++ power-pc.js
@@ -1,6 +1,14 @@
 function ViewModel() {
-    var self = this;
 
+    const buttonPower = document.querySelector('.power');
+    const telaConteudoLigando = document.querySelector('.tela-conteudo-ligando-sistema');
+    const telaConteudoGeral = document.querySelector('.tela-conteudo');
+    const telaLogin = document.querySelector('.conteudo-pc-on-login');
+    const outputElement = document.getElementById('output');
+    const telaAreaTrabalho = document.querySelector('.area-trabalho');
+    const telaLoader = document.querySelector('.tela-loader');
+    var self = this;
+    
     self.telaConteudoLigando = ko.observable('');
 
     const palavras = [
@@ -13,15 +21,13 @@ function ViewModel() {
     ];
 
     self.buttonPower = function() {
-        const buttonPower = document.querySelector('.power');
-        const telaConteudoLigando = document.querySelector('.tela-conteudo-ligando-sistema');
-        const telaConteudoGeral = document.querySelector('.tela-conteudo');
-        const telaLogin = document.querySelector('.conteudo-pc-on-login');
-        const outputElement = document.getElementById('output');
+
         
-        telaConteudoLigando.style.display = 'block';
+        buttonPower.disabled = true;
+        telaConteudoLigando.style.display = 'flex';
         telaConteudoGeral.style.background = '#111';
         buttonPower.style.background = 'aquamarine';
+        buttonPower.style.border = 'none';
         buttonPower.style.boxShadow = 'inset 4px 4px 10px rgba(0, 0, 0, 0.5), inset -4px -4px 10px rgba(255, 255, 255, 0.2)';
 
         // Função para adicionar uma palavra com o efeito de digitação
@@ -44,8 +50,21 @@ function ViewModel() {
         setTimeout(() => {
             telaConteudoLigando.style.display = 'none';
             telaLogin.style.display = 'flex';
+
+            let audio = new Audio('sound/windows-7-startup.mp3');
+            audio.play();
         }, 10000)
     };
+
+    self.buttonEnterLogin = function() {
+        telaLogin.style.display = 'none';
+        telaLoader.style.display = 'flex';
+
+        setTimeout(()=>{
+            telaLoader.style.display = 'none';
+            telaAreaTrabalho.style.display = 'flex'; 
+        },2000)       
+    }
 }
 
 var vm = new ViewModel();
